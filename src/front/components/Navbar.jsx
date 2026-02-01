4988c49dfc7dd4a61e71225c9fffc55c71afa618
Emosoul123 <baurlibennazar@gmail.com>
2026-02-01 00:06:08 +0000

    navbar update

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -13,12 +13,6 @@ export const Navbar = () => {
                 </Link>
                 
                 <div className="ml-auto d-felx gap2">
-
-                    {/* Hamburger */}
-                    <button className="btn btn-primary">
-                    <i className="fa-solid fa-bars"></i>
-                    </button>
-                    
                     {/* Se redirige al usuario a '/demo', que será la página del Carrito */}
                     <Link to="/demo">
                         <button className="btn btn-primary position-relative">
@@ -32,6 +26,65 @@ export const Navbar = () => {
                             )}
                         </button>
                     </Link>
+
+                    {/* Hamburger */}
+                    <button className="btn btn-primary"
+                      type="button"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#mainMenu"
+                      aria-controls="mainMenu">
+                    <i className="fa-solid fa-bars"></i>
+                    </button>
+
+                    <div
+                     className="offcanvas offcanvas-end"
+                    tabIndex="-1"
+                    id="mainMenu"
+                    aria-labelledby="mainMenuLabel"
+                    >
+                  <div className="offcanvas-header">
+                  <h5 className="offcanvas-title" id="mainMenuLabel">Menu</h5>
+                  <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  </div>
+
+                <div className="offcanvas-body">
+                <ul className="navbar-nav">
+                <li className="nav-item">
+                <Link className="nav-link" to="/account" data-bs-dismiss="offcanvas"> 
+                <i className="fa-solid fa-user"></i> My Account
+                </Link>
+                </li>
+
+                <li className="nav-item">
+                <Link className="nav-link" to="/orders" data-bs-dismiss="offcanvas">
+                <i className="fa-solid fa-box"></i> Orders
+                </Link>
+                </li>
+
+                <li className="nav-item">
+                <Link className="nav-link" to="/settings" data-bs-dismiss="offcanvas">
+                <i className="fa-solid fa-gear"></i> Settings
+                </Link>
+                </li>
+                
+                <hr/>
+                <li className="nav-item">
+                <Link className="nav-link text-danger" to="/logout" data-bs-dismiss="offcanvas">
+                <i className="fa-solid fa-right-from-bracket"></i> Logout
+                </Link>
+                </li>
+                </ul>
+
+
+
+
+        </div>
+      </div>
+
+      
+
+
+                    
                 </div>
             </div>
         </nav>
